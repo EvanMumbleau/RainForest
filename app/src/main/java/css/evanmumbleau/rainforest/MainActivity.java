4d094d808933881ac65b3ebd9c93f8ed0ddc7cab
emumbleau <emumbleau@css.edu>
2017-04-30 15:58:06 -0500

    Switch Statements

--- app/src/main/java/css/evanmumbleau/rainforest/MainActivity.java
+++ app/src/main/java/css/evanmumbleau/rainforest/MainActivity.java
@@ -6,11 +6,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     Button buttonAddC;
 
     CheckBox checkbox, checkbox2, checkbox3, checkbox4, checkbox5;
+
+    ArrayList<String> selection = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,19 +29,57 @@ public class MainActivity extends AppCompatActivity {
     }
 
    //maybe...??? (checkbox setup)
-    public void onClicked(View view){
+    public void selectItem(View view){
        //is the view now checked?
-       boolean checked = (CheckBox) view).isChecked();
+       boolean checked = ((CheckBox) view).isChecked();
 
        //Check which checkbox was clicked
        switch(view.getId()){
-           if (checked)
+           case  R.id.checkBox:
+                if(checked)
+                {selection.add("Dell - Inspiron");}
+                        else{
+                    selection.remove("Dell - Inspiron");
+                }
+                    break;
+           case  R.id.checkBox2:
+               if(checked)
+               {selection.add("HP - OMEN");}
+               else{
+                   selection.remove("HP - OMEN");
+               }
+               break;
+           case  R.id.checkBox3:
+               if(checked)
+               {selection.add("Alienware");}
+               else{
+                   selection.remove("Alienware");
+               }
+               break;
+           case  R.id.checkBox4:
+               if(checked)
+               {selection.add("Asus - ROG GL502VM");}
+               else{
+                   selection.remove("Asus - ROG GL502VM");
+               }
+               break;
+           case  R.id.checkBox4:
+               if(checked)
+               {selection.add("MSI");}
+               else{
+                   selection.remove("MSI");
+               }
+               break;
        }
-   }
+       }
 
+    public void finalSelection(View View) {
 
-    private void setupButtonClickEvents() {
+        String final_laptop_selection = "";
 
+        for(String Selections : selection){
+            final_laptop_selection = final_laptop_selection + Selections + "\n";
+        }
     }
 
 }
